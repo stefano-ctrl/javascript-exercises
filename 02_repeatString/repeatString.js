94c7e74b906f2003b228349fdd7b0ce09717f8e2
Stefano <stefanopinna92@hotmail.it>
2022-08-24 14:36:27 +0000

    Five tests passed out of seven

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,6 +2,8 @@ const repeatString = function(string, num) {
      let newString = '';
      for (let i = 0; i < num; i++) {
          newString += string;
+     } if (num < 0) {
+          return 'ERROR';
      }
      return newString;
 };
