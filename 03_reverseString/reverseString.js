424c2810b3507fbb2b247a28c5ab1bb0769c65a9
Stefano <stefanopinna92@hotmail.it>
2022-08-27 14:59:42 +0000

    reverseString tests all passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   string = string.split('');
+   string = string.reverse();
+   string = string.join('');
+   return string;
 };
 
 // Do not edit below this line
