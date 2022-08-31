b24e232aa95e8885f16464244dc1b86f764129c5
Stefano <stefanopinna92@hotmail.it>
2022-08-31 16:00:18 +0000

    Passed all tests. But need to improve code on optional arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, valueToRemove, optionalValue, optionalValue2, optionalValue3) {
+    for (let i = 0; i < array.length; i++) {
+        if (arguments[i] === valueToRemove) {
+            let filteredItems = array.filter(value => ((value !== valueToRemove && value !== optionalValue && value !== optionalValue2 && value !== optionalValue3)));
+            return filteredItems;
+    }
+    
+    }
+}
+ /* array.splice(array.indexOf(value), 1); else if (array[i] !== valueToRemove && array[i] !== optionalValue) { //not working at the same. condition if else might need to be removed
+        return array;
+    } */
 // Do not edit below this line
 module.exports = removeFromArray;
+
+ 
