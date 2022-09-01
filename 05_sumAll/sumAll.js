da2593f81292f762261fdb9b3a2f85542077dd17
Stefano <stefanopinna92@hotmail.it>
2022-09-01 15:10:07 +0000

    Passed all sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (!Number.isInteger(start) || !Number.isInteger(end) ) {
+        return 'ERROR';
+    }
+    else if (end > start) {
+         for (start; start <= end; start++) {
+        sum += start; 
+    } return sum;
+    } else if (start > end) {
+        for (end; end <= start; end++) {
+            sum += end;
+        } return sum; 
+    }
 };
 
 // Do not edit below this line
