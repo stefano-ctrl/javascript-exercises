3376287a519dc340a9b6dfbb1bca9f74d948d3f0
Stefano <stefanopinna92@hotmail.it>
2022-09-03 15:42:59 +0000

    Passed temperature conversions tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+     return Math.round(((far - 32) * 5/9)*10)/10;
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+     return Math.round((cel * 9/5 + 32)*10)/10;
 
 };
 
